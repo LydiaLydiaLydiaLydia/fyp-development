e9c02a5324a04517e0eddd28b169fd245908c613
Student Lydia Sheehan (TL_KCOMP_B_Y4) <t00222338@mymtu.ie>
2026-03-18 14:55:37 +0000

    Added new class for propogration demo and functions to get info about direct and indirect connections for demos

---
+++ ssb-demo-docker/propagation_demo.py
@@ -0,0 +1,31 @@
+class propagation_demo:
+    def __init__(self, ssb_simulator):
+        simulator = ssb_simulator
+        connection_graph = self.get_connection_graph
+        print("okay")
+
+    def get_connection_graph(simulator):
+        graph = {}
+        for node in simulator.nodes:
+            graph[node['name']] = set()
+        
+        for detail in simulator.connection_details:
+            for peer_name in detail['connections']:
+                graph[detail['node']].add(peer_name)
+        
+        return graph
+
+    def classify_nodes(self, author_name: str, graph: dict):
+        direct = {
+            peer for peer in graph[author_name]
+        } | {
+            name for name, peers in graph.items() if author_name in peers
+        }
+        
+        indirect = {
+            node['name'] for node in self.nodes
+            if node['name'] != author_name
+            and node['name'] not in direct
+        }
+        
+        return direct, indirect
