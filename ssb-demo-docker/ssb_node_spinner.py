dafd8b4777091d317fa9050745254d957bdea846
Student Lydia Sheehan (TL_KCOMP_B_Y4) <t00222338@mymtu.ie>
2026-03-18 13:30:31 +0000

    got rid of hash thing in config (unnecessary)

--- ssb-demo-docker/ssb_node_spinner.py
+++ ssb-demo-docker/ssb_node_spinner.py
@@ -307,9 +307,9 @@ class ssb_network_simulator:
                 "host": container_ip,
                 "port": 8008,
                 "allowPrivate": True,
-                "caps": {
-                    "shs": "1KHLiKZvAvjbY1ziZEHMXawbCEIM6qwjCDm3VYRan/s="
-                }
+                #"caps": {
+                #    "shs": "1KHLiKZvAvjbY1ziZEHMXawbCEIM6qwjCDm3VYRan/s="
+                #}
             }
 
             config_json = json.dumps(config, indent=2)
